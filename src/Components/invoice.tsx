dc375e4913e834c8663ddab0ee6535ce21977773
Sofia Niyazson <92554135+sofianiyazson@users.noreply.github.com>
2023-01-03 01:51:26 +0100

    new version

--- src/Components/invoice.tsx
+++ src/Components/invoice.tsx
@@ -1,117 +1,162 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
-import Navbar from "./navbar";
-import {useNavigate} from 'react-router-dom';
+import Navbar from './navbar';
+import { useNavigate } from 'react-router-dom';
+import { Context } from '../Store/context';
 
 function Invoice() {
-    let [totalAmount, setTotalAmount] = useState<any>("");
-    let [totalPaid, setTotalPaid] = useState<any>("");
-    let [invoices, setInvoices] = useState<any>([]);
-    const navigate = useNavigate();
+  let [totalAmount, setTotalAmount] = useState<any>('');
+  let [totalPaid, setTotalPaid] = useState<any>('');
+  let [invoices, setInvoices] = useState<any>([]);
+  const navigate = useNavigate();
 
-    useEffect(() => { getData() }, [])
+  let context = useContext(Context);
 
-    const getData = () => {
-        axios.get('http://localhost:3000/invoices')
-            .then(res => {
-                setInvoices(res.data);
+  useEffect(() => {
+    context.getInvoices();
+    setInvoices(context.invoices);
+    processData(context.invoices);
+  }, [context.invoices.length]);
 
-                let t1 = 0;
-                let t2 = 0;
-                res.data.forEach((elem:any) => {
-                    t1 += parseInt(elem.amount);
-                });
-                setTotalAmount(t1.toFixed(2));
+  const processData = (data: any) => {
+    setInvoices(data);
 
-                let paid = res.data.filter((elem:any) => elem.status === "PAID");
-                paid.forEach((elem:any) => {
-                    t2 += parseInt(elem.amount);
-                });
-                setTotalPaid(t2.toFixed(2));
-            })
-            .catch(err => console.log(err))
+    let t1 = 0;
+    let t2 = 0;
+    data.forEach((elem: any) => {
+      t1 += parseInt(elem.amount);
+    });
+    setTotalAmount(t1.toFixed(2));
+
+    let paid = data.filter((elem: any) => elem.status === 'PAID');
+    paid.forEach((elem: any) => {
+      t2 += parseInt(elem.amount);
+    });
+    setTotalPaid(t2.toFixed(2));
+  };
+
+  const updateStatus = (obj: any) => {
+    if (window.confirm('Are you sure and want to update status as "PAID"?')) {
+      axios
+        .put(`http://localhost:3000/invoices/${obj.id}`, {
+          status: 'PAID',
+          createdDate: obj.createdDate,
+          dueDate: obj.dueDate,
+          customerName: obj.customerName,
+          amount: obj.amount,
+          tasks: obj.tasks,
+          project: obj.project,
+        })
+        .then((res) => {
+          window.location.reload();
+        })
+        .catch((err) => console.log(err));
     }
+  };
 
-    const updateStatus = (obj:any) => {
-        if(window.confirm('Are you sure and want to update status?')) {
-            axios.put(`http://localhost:3000/invoices/${obj.id}`, {
-                status: "PAID",
-                createdDate: obj.createdDate,
-                dueDate: obj.dueDate,
-                amount: obj.amount,
-                customerName: obj.customerName,
-            })
-            .then(res => { getData() })
-            .catch(err => console.log(err))
-        }
+  const delElem = (id: any) => {
+    if (window.confirm('Are you sure and want to delete this invoice?')) {
+      axios
+        .delete(`http://localhost:3000/invoices/${id}`)
+        .then((res) => {
+          context.getInvoices();
+        })
+        .catch((err) => console.log(err));
     }
+  };
 
-    const delElem = (id:any) => {
-        if(window.confirm('Are you sure and want to update status?')) {
-            axios.delete(`http://localhost:3000/invoices/${id}`)
-            .then(res => { getData() })
-            .catch(err => console.log(err))
-        }
-    }
+  const view = (id: any) => {
+    navigate(`/invoiceDetails?id=${id}`);
+  };
 
-    const view = (id:any) => {
-        navigate(`/invoiceDetails?id=${id}`);
-    }
+  return (
+    <>
+      <div className="row header">
+        <div className="col">
+          <h1 className="m-0 p-4">Invoices</h1>
+          <nav className="navbar fixed navbar-dark overviewNavbar">
+            <a className="navbar-brand mx-auto">
+              <h4>Total: ${totalAmount}</h4>
+            </a>
+            <a className="navbar-brand mx-auto">
+              <h4>Paid: ${totalPaid}</h4>
+            </a>
+            <a className="navbar-brand mx-auto">
+              <h4>Unpaid: ${totalAmount - totalPaid}.00</h4>
+            </a>
+          </nav>
+        </div>
+      </div>
 
-    return (
-        <>
-            <div className='row header'>
-                <div className='col'>
-                    <h1 className='m-0 p-4'>Invoices</h1>
-                    <nav className="navbar fixed navbar-dark overviewNavbar">
-                        <a className="navbar-brand mx-auto"><h4>Total: ${totalAmount}</h4></a>
-                        <a className="navbar-brand mx-auto"><h4>Paid: ${totalPaid}</h4></a>
-                        <a className="navbar-brand mx-auto"><h4>Unpaid: ${totalAmount - totalPaid}</h4></a>
-                    </nav>
-                </div>
-            </div>
-
-            <div className="row">
-                <div className="col m-4 table-responsive">
-                    <table className="table table-hover table-bordered table-striped table-sm dashboardTable">
-                        <thead>
-                            <tr>
-                                <th>Sr. No.</th>
-                                <th>Customer Name</th>
-                                <th>Amount</th>
-                                <th>Issue Date</th>
-                                <th>Due Date</th>
-                                <th>Status</th>
-                                <th>View</th>
-                                <th>Options</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {invoices.map((elem:any, idx:any) => {
-                                return <tr key={idx}>
-                                    <td>{idx+1}</td>
-                                    <td>{elem.customerName}</td>
-                                    <td>{parseInt(elem.amount).toFixed(2)}</td>
-                                    <td>{elem.createdDate}</td>
-                                    <td>{elem.dueDate}</td>
-                                    <td>{elem.status === "PAID" ? <span className="badge bg-success">{elem.status}</span> :
-                                    <span className="badge bg-secondary">{elem.status}</span>}</td>
-                                    <td>
-                                        <button className="btn btn-dark btn-sm mx-1" onClick={() => view(elem.id)}>View</button>
-                                    </td>
-                                    <td>
-                                        <button className="btn btn-dark btn-sm mx-1" onClick={() => updateStatus(elem)} disabled={elem.status !== "NOT PAID"}>Mark As Paid</button>
-                                        <button className="btn btn-danger btn-sm mx-1" onClick={() => delElem(elem.id)}>Delete</button>
-                                    </td>
-                                </tr>
-                            })}
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-          <Navbar />
-        </>
-    )
+      <div className="row">
+        <div className="col m-4 table-responsive">
+          <table className="table table-hover table-bordered table-striped table-sm dashboardTable">
+            <thead>
+              <tr>
+                <th>Sr. No.</th>
+                <th>Customer Name</th>
+                <th>Project Name</th>
+                <th>Amount</th>
+                <th>Issue Date</th>
+                <th>Due Date</th>
+                <th>Status</th>
+                <th>View</th>
+                <th>Options</th>
+              </tr>
+            </thead>
+            <tbody>
+              {invoices.map((elem: any, idx: any) => {
+                return (
+                  <tr key={idx}>
+                    <td>{idx + 1}</td>
+                    <td>{elem.customerName}</td>
+                    <td>{elem.project.projectName}</td>
+                    <td>${parseInt(elem.amount).toFixed(2)}</td>
+                    <td>{elem.createdDate}</td>
+                    <td>{elem.dueDate}</td>
+                    <td>
+                      {elem.status === 'PAID' ? (
+                        <span className="badge bg-success">{elem.status}</span>
+                      ) : (
+                        <span className="badge bg-secondary">
+                          {elem.status}
+                        </span>
+                      )}
+                    </td>
+                    <td>
+                      <button
+                        className="btn btn-dark btn-sm mx-1"
+                        onClick={() => view(elem.id)}
+                      >
+                        View
+                      </button>
+                    </td>
+                    <td>
+                      <button
+                        className="btn btn-dark btn-sm mx-1"
+                        onClick={() => updateStatus(elem)}
+                        disabled={elem.status !== 'NOT PAID'}
+                      >
+                        Mark As Paid
+                      </button>
+                      <button
+                        className="btn btn-danger btn-sm mx-1"
+                        onClick={() => delElem(elem.id)}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+        {invoices.length === 0 && <h6>There are no invoices created yet.</h6>}
+      </div>
+      <Navbar />
+    </>
+  );
 }
 
 export default Invoice;
--- src/Components/invoiceDetails.tsx
+++ src/Components/invoiceDetails.tsx
@@ -1,22 +1,31 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
-import Navbar from './navbar';
 import moment from 'moment';
+import { Context } from '../Store/context';
+import { Link } from 'react-router-dom';
 
 function InvoiceDetails() {
   let [elem, setElem] = useState<any>({});
+  let [totalTime, setTotalTime] = useState<any>('');
 
   useEffect(() => {
-    getData();
+    getInvoiceData();
   }, []);
 
-  const getData = () => {
+  const getInvoiceData = () => {
     let id = window.location.href.split('id=').pop();
     axios
       .get(`http://localhost:3000/invoices/${id}`)
       .then((res) => {
-        console.log(res.data);
         setElem(res.data);
+        console.log(res.data);
+        let k = 0;
+        res.data.tasks.forEach((elem: any) => {
+          let myVar = moment.duration(elem.time).asSeconds(); //mm:ss to seconds
+          k += myVar;
+        });
+        let p = moment.utc(k * 1000).format('HH:mm:ss');
+        setTotalTime(p);
       })
       .catch((err) => console.log(err));
   };
@@ -35,19 +44,22 @@ function InvoiceDetails() {
           Customer Name: <b>{elem.customerName}</b>
         </p>
         <p>
+          Status: <b>{elem.status}</b>
+        </p>
+        <p>
           Issue Date: <b>{elem.createdDate}</b>
         </p>
         <p>
           Due/Expiry Date: <b>{elem.dueDate}</b>
         </p>
         <p>
-          Task Creation Date: <b>{elem.taskCreationDate}</b>
-        </p>
-        <p>
-          Status: <b>{elem.status}</b>
-        </p>
-        <p>
           Invoice Id: <b>{elem.id}</b>
+        </p>
+        <p>
+          Project Name: <b>{elem.project && elem.project.projectName}</b>
+        </p>
+        <p>
+          Project Creation Date: <b>{elem.project && elem.project.date}</b>
         </p>
       </div>
 
@@ -63,24 +75,42 @@ function InvoiceDetails() {
               </tr>
             </thead>
             <tbody>
+              {elem.tasks &&
+                elem.tasks.length > 0 &&
+                elem.tasks.map((e: any, idx: any) => {
+                  return (
+                    <tr key={idx}>
+                      <td>{e.taskName}</td>
+                      <td>${e.invoice}</td>
+                      <td>{e.time}</td>
+                      <td>${e.amount}</td>
+                    </tr>
+                  );
+                })}
               <tr>
-                <td>{elem.taskName}</td>
-                <td>${elem.invoice}</td>
-                <td>{elem.time}</td>
-                <td>${parseFloat(elem.amount).toFixed(2)}</td>
-              </tr>
-              <tr>
-                <td colSpan={3}>
+                <td colSpan={2}>
                   <b>Total</b>
                 </td>
                 <td>
-                  <b>${parseFloat(elem.amount).toFixed(2)}</b>
+                  <b>{totalTime}</b>
+                </td>
+                <td>
+                  <b>${elem.amount}</b>
                 </td>
               </tr>
             </tbody>
           </table>
         </div>
-        <h5> </h5>
+        <h5>Invoice</h5>
+      </div>
+      <div className="col-12">
+        <Link
+          to="/invoice"
+          className="btn btn-sm btn-dark m-3"
+          style={{ float: 'left' }}
+        >
+          Go Back
+        </Link>
       </div>
     </div>
   );
