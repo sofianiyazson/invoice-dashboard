a3b1e1724b37dc8d7ab383ab235daeca6382a602
Sofia Niyazson <92554135+sofianiyazson@users.noreply.github.com>
2023-01-03 02:01:05 +0100

    updated context

--- src/Store/context.tsx
+++ src/Store/context.tsx
@@ -1,20 +1,45 @@
 import React, { Component, createContext, useEffect, useState } from 'react';
 import axios from 'axios';
+import Projects from '../Components/projects';
 export const Context = React.createContext<any>(null);
 
 function ThemeContextProvider(props:any) {
-    let [data, setData] = useState<any[]>([]);
+    let [tasks, setTasks] = useState<any[]>([]);
+    let [projects, setProjects] = useState<any[]>([]);
+    let [invoices, setInvoices] = useState<any>([]);
 
-    useEffect(() => {
+    const getTasks = () => {
         axios.get('http://localhost:3000/tasks')
             .then(res => {
-                setData(res.data);
+                setTasks(res.data);
             })
             .catch(err => console.log(err));
-    }, [])
+    }
 
+    const getProjects = () => {
+        axios.get('http://localhost:3000/projects')
+            .then(res => {
+                setProjects(res.data);
+            })
+            .catch(err => console.log(err));
+    }
+
+    const getInvoices = () => {
+        axios.get('http://localhost:3000/invoices')
+            .then(res => {
+                setInvoices(res.data);
+            })
+            .catch(err => console.log(err));
+    }
+
+    useEffect(() => {
+        getTasks();
+        getProjects();
+        getInvoices();
+    },[])
+    
     return (
-        <Context.Provider value={{ data }}>
+        <Context.Provider value={{ tasks, projects, invoices, getTasks, getProjects, getInvoices}}>
             {props.children}
         </Context.Provider>
     )
